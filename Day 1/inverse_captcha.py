a917f9535a1ba28a880e49e9b3daa2b0e6366919
Adri <adriangalende@gmail.com>
2017-12-10 13:34:24 +0200

    commit inicial

---
+++ Day 1/inverse_captcha.py
@@ -0,0 +1,15 @@
+# --- Dia 1: Inverse Captcha - --
+#
+# El captcha requiere que revises una secuencia de digitos (la entrada de tu puzzle)
+# y encuentres la suma de todos los digitos que coincidan con el siguiente digito de la lista.
+# La lista es circular, asi que el digito de despues del ultimo es el primer digito de la lista.
+#
+# Por ejemplo:
+#
+# 1122 produce la suma de 3 (1 + 2) porque el primer digito (1)  coincide con el segundo digito
+# y el tercer digito (2) coincide con el cuarto.
+# 1111 produce 4 porque cada digito (todos 1) coinciden con el siguiente.
+# 1234 produce 0 porque ninguno de los digito coincide con el siguiente.
+# 91212129 produce 9 porque el unico digito que coincide con el siguiente es el ultimo (9).
+#
+#¿Cual es la solucion al captcha?
